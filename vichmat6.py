19d0ffa789e6668adc28b0082723da8f94238483
alexpskovalex <lha0002000@gmail.com>
2021-09-26 17:00:29 +0300

    6 испр

--- vichmat6.py
+++ vichmat6.py
@@ -1,26 +1,33 @@
-a = [[1.5, 3.9, 0.8, 9], [2.3, -3, 4.1, -8.2], [-2, 5, -0.9, 7.6], [1.8, 0.2, 6.2, -3]]
-b = [-0.3, 1.5, 9, 0.1]
+a = [[1.5, 3.9, 0.8, 9], [ 2.3, -3, 4.1, -8.2], [4.4, 5, -0.9, 7.6], [1.8, 0.2, 6.2, -3]]
+b = [ -0.3, 1.5, 9, 0.1]
 n = 4
-x=[0,0,0,0]
-for s in range(0,n-1):# 1-8
-     for j in range(s,n):#2
-          a[s][j]/=a[s][s]#2
-     b[s]/=a[s][s]#3
-     for i in range(s+1,n):#4-7
-          for j in range(s,n):#5
-               a[i][j]-=a[i][s]*a[s][j]#5
-          b[i]-=b[s]*a[i][s]#6
-x[n-1]=b[n-1]/a[n-1][n-1]#9
-# print(s,' ',n-2)
-for s in range(n-2,-1,-1):#10-12
-     # print(s)
-     x[s]=b[s]
-     # print('s={0}'.format(s))
-     for k in range(s+1,n):
-          x[s]-=a[s][k]*x[k]
-          # print('a[{1}][{2}] = {0:7.2f}'.format(a[s][k],s+1,k+1))
-for m in range(0,4):
-          print('{0:5.2f} {1:5.2f} {2:5.2f} {3:5.2f} | {4:5.2f}'.format(a[m][0],a[m][1],a[m][2],a[m][3],b[m]))
+x = [0, 0, 0, 0]
+print('дано:')
+for m in range(0, n):
+    print('{0:5.2f} {1:5.2f} {2:5.2f} {3:5.2f} | {4:5.2f}'.format(a[m][0], a[m][1], a[m][2], a[m][3], b[m]))
 print('-----------')
-for m in range(0,4):
-     print('x[{0}] = {1:7.2f}'.format(m,x[m]))
+
+for s in range(0,n):
+     for i in range(s+1,n):
+          k=a[i][s]/a[s][s]
+          for j in range(s,n):
+               a[i][j]-=k*a[s][j]
+          b[i]-=k*b[s]
+          
+for i in range(0,n):
+     b[i]/=a[i][i]
+     for j in range(n-1,i-1,-1):
+          a[i][j]/=a[i][i]
+
+for s in range (n-1,-1,-1):
+     x[s]=b[s]
+     for k in range(n-1,s,-1):
+          x[s]-=a[s][k]*x[k]
+print('-после преобразований:')
+for m in range(0, n):
+    print('{0:5.2f} {1:5.2f} {2:5.2f} {3:5.2f} | {4:5.2f}'.format(a[m][0], a[m][1], a[m][2], a[m][3], b[m]))
+print('-----------')
+print('-ответы-')
+for m in range(0, 4):
+    print('x[{0}] = {1:7.2f}'.format(m, x[m]))
+print('-----------')
